a5ea9a6303d15b5480a889a3d1ca1fad6ecf6288
nclaihan <nclaihan@foxmail.com>
2024-08-13 20:03:22 +0300

    Adjusting test cases

--- subtitle_trans/extractor.py
+++ subtitle_trans/extractor.py
@@ -9,34 +9,41 @@ print_progress = True
 # Extractor for subtitles from audio
 class Extractor:
     device: str
-    batch_size: int
+    device_index: int
     compute_type: str
     download_root: str
-    whisperx_model: FasterWhisperPipeline
 
     def __init__(self,
                  # cuda or cpu
                  device="cpu",
                  device_index=0,
-                 # float16 or int8
-                 compute_type="int8",
-                 # large-v2 or large-v3
-                 model="large-v2"):
+                 # float16 or int8 or int8_float16
+                 compute_type="int8", ):
         self.device = device
+        self.device_index = device_index
         self.compute_type = compute_type
         self.download_root = f"{Path.home()}/models"
-        self.whisperx_model = whisperx.load_model(model, device, device_index=device_index, compute_type=compute_type,
-                                                  download_root=self.download_root, threads=16)
 
-    def transcribe(self, audio_file: str, batch_size: int, use_auth_token: str):
+    def transcribe(self, audio_file: str, batch_size: int,
+                   # large-v2 or large-v3
+                   model="large-v3"):
+        whisperx_model = whisperx.load_model(model, self.device, device_index=self.device_index,
+                                             compute_type=self.compute_type,
+                                             download_root=self.download_root, threads=batch_size)
         audio = whisperx.load_audio(audio_file)
-        result = self.whisperx_model.transcribe(audio, batch_size=batch_size, print_progress=print_progress,
-                                                combined_progress=print_progress)
-        model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=self.device,
+        return whisperx_model.transcribe(audio, batch_size=batch_size, print_progress=print_progress)
+
+    def align(self, audio_file: str, transcribe_result):
+        model_a, metadata = whisperx.load_align_model(language_code=transcribe_result["language"], device=self.device,
                                                       model_dir=self.download_root)
-        result = whisperx.align(result["segments"], model_a, metadata, audio, self.device, return_char_alignments=False,
-                                print_progress=print_progress, combined_progress=print_progress)
-        diarize_model = whisperx.DiarizationPipeline(use_auth_token=use_auth_token,
-                                                     device=self.device)
-        diarize_segments = diarize_model(audio)
-        return whisperx.assign_word_speakers(diarize_segments, result)
+        audio = whisperx.load_audio(audio_file)
+        return whisperx.align(transcribe_result["segments"], model_a, metadata, audio, self.device,
+                              return_char_alignments=False,
+                              print_progress=print_progress)
+
+    def classify(self, audio_file: str, use_auth_token: str, transcribe_result):
+        model = whisperx.DiarizationPipeline(use_auth_token=use_auth_token,
+                                             device=self.device)
+        audio = whisperx.load_audio(audio_file)
+        segments = model(audio)
+        return whisperx.assign_word_speakers(segments, transcribe_result)
--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,7 +1,17 @@
+import json
+
 from subtitle_trans.extractor import Extractor
 
 
 def test_transcribe():
     extractor = Extractor()
-    result = extractor.transcribe("test_audio.mp3", 16, "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")
-    print(result)
+    audio_file = "test_audio_cn.mp3"
+    result = extractor.transcribe(audio_file, 16)
+    print(json.dumps(result))
+    result = extractor.align(audio_file, result)
+    print(json.dumps(result))
+    result = extractor.classify(audio_file, "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", result)
+    print(json.dumps(result))
+
+
+test_transcribe()
